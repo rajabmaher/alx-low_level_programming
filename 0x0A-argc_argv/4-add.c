346dde3e93ebd106986e0db50830f68be2b0a336
rajabmaher <122832718+rajabmaher@users.noreply.github.com>
2024-04-05 07:39:12 -0700

    next projects

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
-	{
-		/*Declaring variables*/
-		unsigned int count;
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+* check_num - check - string there are digit
+* @str: array str
+* Return: Always 0 (Success)
+**/
+int check_num(char *str)
+{
+/*Declaring variables*/
+unsigned int count;
 
 		count = 0;
 		while (count < strlen(str)) /*count string*/
